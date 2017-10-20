771c8a106145f6903e03f6c958ce0f8536a9798b
Benchekroun Hamza <hbenchekro@TC405-109-14.insa-lyon.fr>
2017-10-20 15:50:01 +0200

    Modification check.c

--- check.c
+++ check.c
@@ -1,68 +1,68 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int checkSudoku(int sudoku[9][9]){
+   
     int check = 1;
+    int i,j,k,l,m,n;
     
-    for(int i=0; i < 9; i++){
-        for(int j=0; j < 9; j++){
-            for(int k=0; k < 9; k++){
-                if(k != j || sudoku[i][j]==sudoku[i][k])
+    for(i=0; i < 9; i++){
+        for(j=0; j < 9; j++){
+            for(k=0; k < 9; k++){
+                if(k != j && sudoku[i][j]==sudoku[i][k])
                 check = 0;
-                if(k != j || sudoku[j][i]==sudoku[k][i])
+                if(k != j && sudoku[j][i]==sudoku[k][i])
                 check = 0;
-            }
-        }
-
-    }
-    
-    for(int i=0; i < 3; i++){
-        for(int j=0; j < 3; j++){
-            for(int k=0; k < 3; k++){
-                for(int l=0; l < 3; l++){
-                    for(int m=0; m < 3; m++){
-                        for(int n=0; n < 3; n++){
-                            if(k != m || l != n || sudoku[i+k][j+m]==sudoku[i+l][j+n])
-                            check = 0;
-                        }
-                    }
-                }
             }
         }
     }
 
-    
+    for(i=0; i < 3;i++){
+        for(j=0; j < 3; j++){
+             for(k=0;k < 3;k++){
+                 for(l=0;l < 3;l++){
+                      for(m=0;m < 3;m++){
+                          for(n=0;n < 3;n++){
+                              if(k != m && l != n && sudoku[i+k][j+l] == sudoku[i+m][j+n])
+                                  check = 0;
+                          }
+                      }
+                 }
+             }
+        }
+    }
+
     return check;
 
 }
 
+int lireSudoku(FILE *fich,int sudoku[9][9]){
+    int i,j,res;
+    int val; 
+    for (i=0; i<9; i++) 
+             for (j=0; j<9; j++) {  
+                 res=fscanf(fich, "%d",&val);
+                 if (res==EOF){
+                     fprintf(stderr,"Fin de ficher atteinte: manque des coefficients\n");
+                      exit(-1);
+                 }
+                 sudoku[i][j]=val;
+             }  
+    return(0);
+}
+
+
+
 int main() {
 
-    int sudoku[9][9] = 
-        {
-            {9,0,0,1,0,0,0,0,5},
-            {0,0,5,0,9,0,2,0,1},
-            {8,0,0,0,4,0,0,0,0},
-            {0,0,0,0,8,0,0,0,0},
-            {0,0,0,7,0,0,0,0,0},
-            {0,0,0,0,2,6,0,0,9},
-            {2,0,0,3,0,0,0,0,6},
-            {0,0,0,2,0,0,9,0,0},
-            {0,0,1,9,0,4,5,7,0}
-            
-        };
+    int sudoku[9][9];
+    FILE *fich = fopen("sudoku1.txt","r");
+
+    lireSudoku(fich,sudoku);
     
     int a = checkSudoku(sudoku);
-    printf("%d",a);
+    printf("%d\n",a);
 
     return 0;
 
